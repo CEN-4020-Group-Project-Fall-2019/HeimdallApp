bbfe036ab9d7b88de73ba79f2d62488221897d87
KatherineSkipper <54860399+KatherineSkipper@users.noreply.github.com>
2019-10-20 11:16:33 -0400

    Updated 3 files ~Katie
    Updated Recommendation.java, Settings.java, and AddTwitter.java with some of Jacob's additional features to mesh our ideas until we can get together in person.

--- Heimdall/app/src/main/java/com/example/heimdall/AddTwitter.java
+++ Heimdall/app/src/main/java/com/example/heimdall/AddTwitter.java
@@ -1,11 +1,11 @@
-package com.example.heimdall;
+package com.example.add_twiter_follow;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class AddTwitter extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +13,27 @@ public class AddTwitter extends AppCompatActivity {
         setContentView(R.layout.activity_add_twitter);
     }
 
+    public void toAddTwitter(View view) {
+        //go to addTwitter page
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    //Added to keep some of Jacob's functionality
     public void backScreen(View view){
         Intent intent = new Intent(this, CardInfo.class);
+        startActivity(intent);
+    }
+
+    public void toRecomend(View view) {
+        //go to Recomendations page
+        Intent intent = new Intent(this, recommendations.class);
+        startActivity(intent);
+    }
+
+    public void toSettings(View view) {
+        //go to Settings page
+        Intent intent = new Intent(this, settings.class);
         startActivity(intent);
     }
 }
--- Heimdall/app/src/main/java/com/example/heimdall/Settings.java
+++ Heimdall/app/src/main/java/com/example/heimdall/Settings.java
@@ -1,20 +1,48 @@
-package com.example.heimdall;
+package com.example.add_twiter_follow;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class Settings extends AppCompatActivity {
+import com.example.add_twiter_follow.ui.main.SectionsPagerAdapter;
+
+public class settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
     }
 
+    //Added to keep some of Jacob's functionality
     public void backScreen(View view){
         Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
+    public void toAddTwitter(View view) {
+        //go to addTwitter page
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void toRecomend(View view) {
+        //go to Recomendations page
+        Intent intent = new Intent(this, recommendations.class);
+        startActivity(intent);
+    }
+
+    public void toSettings(View view) {
+        //go to Settings page
+        Intent intent = new Intent(this, settings.class);
         startActivity(intent);
     }
 }
