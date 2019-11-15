d4262ec81067cd57e50db3ad8f38939d92e8d4c6
dienger <54902587+dienger@users.noreply.github.com>
2019-11-15 02:49:33 -0500

    Password reset through email address now works
    What is your mother's maiden name?
    What color was your first car?
    What is the wind speed velocity of an unladen swallow?

--- Heimdall/app/src/main/java/com/example/heimdall/ForgotPassword.java
+++ Heimdall/app/src/main/java/com/example/heimdall/ForgotPassword.java
@@ -3,18 +3,38 @@ package com.example.heimdall;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void backScreen(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void resetPassword(View view){
+        TextView em = findViewById(R.id.PWRemail);
+
+        Log.d("HEIMDALL", "email: " + em.getText().toString());
+        if(em.getText().length() > 0) {
+            String userEmail = em.getText().toString();
+            //TODO tell the user if email wasn't sent
+            mAuth.sendPasswordResetEmail(userEmail);
+        }else{
+            Log.d("HEIMDALL", "no email provided");
+        }
     }
 }
