7fc986ade3e05f2eb56f9bfaba5f4d54f42157ab
dienger <54902587+dienger@users.noreply.github.com>
2019-12-04 14:25:38 -0500

    Recommendations are now pulled from the first 10 in database
    Fricken gnarly dude

--- Heimdall/app/src/main/java/com/example/heimdall/Recommendation.java
+++ Heimdall/app/src/main/java/com/example/heimdall/Recommendation.java
@@ -13,8 +13,13 @@ import android.support.v4.view.PagerAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Iterator;
 
 //import com.example.add_twiter_follow.ui.main.SectionsPagerAdapter;
 
@@ -26,19 +31,33 @@ public class Recommendation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendation);
         db = FirebaseDatabase.getInstance();
-        DatabaseReference recRef = db.getReference("calls");
-        parentLayout = findViewById(R.id.RecLayout);
-        final LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View row = inflater.inflate(R.layout.field3, parentLayout, false);
-        String stk = "Stock";
-        for(int i = 287; i < 297; ++i){
-            TextView tmp = (TextView) row;
-            tmp.setId(i);
-            tmp.setText(stk + i);
-            parentLayout.addView(row);
-            row = inflater.inflate(R.layout.field3, parentLayout, false);
+        DatabaseReference recRef = db.getReference("recommended");
+        recRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> datas = dataSnapshot.getChildren();
+                parentLayout = findViewById(R.id.RecLayout);
+                final LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View row = inflater.inflate(R.layout.field3, parentLayout, false);
+                String stk = "";
+                int i = 297;
+                for(Iterator<DataSnapshot> itr = datas.iterator(); i < 307; ++i){
+                    stk = itr.next().getKey();
+                    TextView tmp = (TextView) row;
+                    tmp.setId(i);
+                    tmp.setText(stk);
+                    parentLayout.addView(row);
+                    row = inflater.inflate(R.layout.field3, parentLayout, false);
 
-        }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     //Added to keep some of Jacob's functionality
