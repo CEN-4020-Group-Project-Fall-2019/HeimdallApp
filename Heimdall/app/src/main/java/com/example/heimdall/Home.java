5b9cc6c7c9256e54d6b4bdf5e16eb9b59215b1f9
dienger <54902587+dienger@users.noreply.github.com>
2019-12-06 01:10:59 -0500

    Added documentation and cleaned up home.class code
    I'm really bad at doing documentation while coding. Sorry.

--- Heimdall/app/src/main/java/com/example/heimdall/Home.java
+++ Heimdall/app/src/main/java/com/example/heimdall/Home.java
@@ -39,73 +39,33 @@ public class Home extends AppCompatActivity {
         parentLayout = findViewById(R.id.VerticalLayout);
         mAuth = FirebaseAuth.getInstance();
         wList = new Vector<String>();
+
+        //Function to load in the current user's watchlist
         loadInCalls(this.getCurrentFocus());
     }
 
-    public void backScreen(View view){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-
-        //TODO: add logout functionality
-        mAuth.signOut();
-    }
-
-    public void addScreen(View view){
-        /*AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("New dialog");
-        builder.setTitle("Test");
-
-        AlertDialog dialog = builder.create();
-
-        dialog.show();
-
-        return 1;*/
-        //Intent intent = new Intent(this, AddCard.class);
-        //startActivity(intent);
-    }
-
-    public void onAddField(View v) {
-
-        Intent intent = new Intent(this, AddCard.class);
-        startActivity(intent);
-
-        //View row = inflater.inflate(R.layout.field, null);
-       /* TextView t = findViewById(R.id.textView10);
-
-        if(findViewById(R.id.textView10).getTag().equals("none")){
-            t.setText("STK" + selection +"(x.x%)");
-            t.setTag("");
-        }else {
-            row.setId(rowCount);
-            row.setLayoutParams(t.getLayoutParams());
-            TextView tmp = (TextView) row;
-            tmp.setText("NSTK" + selection +"(x.x%)");
-            parentLayout.addView(row, parentLayout.getChildCount());
-            ++rowCount;
-        }*/
-    }
-
     public void loadInCalls(View v){
-
+        //Create variables for the current user id, dynamic view adder, and database refs
         final String currentUser = mAuth.getUid();
         final LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("watchList/");
 
+        //Event listeners are ways to access database
+        //Functions defined inside are run when the event
+        //is triggered
         myRef.addValueEventListener(new ValueEventListener() {
-
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
-                // whenever data at this location is updated. I am not accounting
-                // for removing a stock from the homepage because it is auto updated
-                // every time the homepage is loaded and an update from the homepage
-                // shouldn't happen.
+                // whenever data at this location is updated.
                  View row = inflater.inflate(R.layout.field, parentLayout, false);
                  Iterable<DataSnapshot> datas = dataSnapshot.child(currentUser).child("Watchlist").getChildren();
                  String stk = "";
                  int i = 176;
 
+                 //Going through the data snapshot and adding a row for each stock
+                //in user's watchlist
                 for (Iterator<DataSnapshot> itr = datas.iterator(); itr.hasNext(); ++i) {
                     stk = itr.next().getKey();
                     if (!wList.contains(stk)) {
@@ -127,6 +87,17 @@ public class Home extends AppCompatActivity {
         });
     }
 
+    public void backScreen(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        mAuth.signOut();
+    }
+
+    public void onAddField(View v) {
+        Intent intent = new Intent(this, AddCard.class);
+        startActivity(intent);
+    }
+
     public void infoScreen(View view){
         TextView callingObject = findViewById(view.getId());
         Intent intent = new Intent(this, company_info.class);
@@ -139,21 +110,17 @@ public class Home extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void recommendationsScreen(View view){
-        Intent intent = new Intent(this, Recommendation.class);
-        startActivity(intent);
+    public void notificationSystem(View view) {
+        //Intent intent = new Intent(this, Home.class);
+        //PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        //Building notifications
+        //NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.CHANNEL_ID))
+        //.setSmallIcon(/*not sure what to put here or if we can ignore it*/)
+        //        .setContentTitle("TitleToBe")
+        //        .setContentText("MSG to user here")
+        //        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        //        //.setContentIntent(pendingIntent) //intent to open upon user click...probably don't need this...
+        //        .setAutoCancel(true);
     }
-
-    //Intent intent = new Intent(this, Home.class);
-    //PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-    //Building notifications
-    //NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.CHANNEL_ID))
-            //.setSmallIcon(/*not sure what to put here or if we can ignore it*/)
-    //        .setContentTitle("TitleToBe")
-    //        .setContentText("MSG to user here")
-    //        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-    //        //.setContentIntent(pendingIntent) //intent to open upon user click...probably don't need this...
-    //        .setAutoCancel(true);
-
 
 }
