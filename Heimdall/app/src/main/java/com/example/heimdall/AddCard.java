f86aa34e035186729a15f33bb3f34cc0dff21f1b
dienger <54902587+dienger@users.noreply.github.com>
2019-12-06 01:19:18 -0500

    Added documentation and cleaned up code for AddCard class
    Everyone likes toast.

--- Heimdall/app/src/main/java/com/example/heimdall/AddCard.java
+++ Heimdall/app/src/main/java/com/example/heimdall/AddCard.java
@@ -40,13 +40,13 @@ public class AddCard extends AppCompatActivity {
     String currentUser = "";
     Vector<String> dbStocks = new Vector<String>();
 
-    boolean firstLoad = true;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         context = getApplicationContext();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        //Setting up variables for later use
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getUid();
         findViewById(R.id.addToWatchlist).setOnClickListener(new View.OnClickListener() {
@@ -57,56 +57,70 @@ public class AddCard extends AppCompatActivity {
                 toast.show();
             }
         });
+
+        //Load in all the entries in the stocks database
+        //whenever the page loads
         loadValues();
     }
 
     public void loadValues(){
+        //Get the database reference and access through
+        //event listeners
         final DatabaseReference stocksRef = database.getReference("stocks");
         stocksRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Iterable<DataSnapshot> datas = dataSnapshot.getChildren();
                 String stk = "";
+
+                //Iterate through each child and get the stock name
+                //If the stock name has not been added yet, then add
+                //the stock
                 for (Iterator<DataSnapshot> itr = datas.iterator(); itr.hasNext(); ) {
                     stk = itr.next().getKey();
                     if (!dbStocks.contains(stk)) {
                         dbStocks.add(stk);
                     }
                 }
+
+                //This code controls the add to watchlist button
+                //You aren't allowed to add a stock until the db
+                //completely loads, which this enforces
                 findViewById(R.id.addToWatchlist).setAlpha(1);
+
+                //Now the button will call the add function whenever
+                //it is clicked
                 findViewById(R.id.addToWatchlist).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         addStockCard(getCurrentFocus());
                     }
                 });
-                Log.d("HEIMDALL", "running the thingy");
+
+                //Tell the user that they can add stocks to watchlist now
                 toastMessage = "Database finished loading. Feel free to enter values now.";
                 Toast toast = Toast.makeText(context, toastMessage, duration);
                 toast.show();
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
         });
 
 
     }
 
-    public void backScreen(View view){
-        Intent intent = new Intent(this, Home.class);
-        startActivity(intent);
-    }
-
     //add function that calls info from Heimdallcen4020 and puts stock into UserDB: watchlist
     public void addStockCard(View view){
+        //Setting up variables
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_LONG;
         TextView userEntry = findViewById(R.id.followStock);
         stockToFind = userEntry.getText().toString().toUpperCase();
 
+        //This checks whether or not the user entered stock symbol
+        //is in the database. If it is, then it is added to the
+        //user's watchlist, if not, then the user is told
         if(dbStocks.contains(stockToFind)){
             wlRef.child(currentUser).child("Watchlist").child(stockToFind).setValue("stck added");
             toastMessage = stockToFind + " added successfully!";
@@ -118,5 +132,10 @@ public class AddCard extends AppCompatActivity {
             toast.show();
         }
 
+    }
+
+    public void backScreen(View view){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
